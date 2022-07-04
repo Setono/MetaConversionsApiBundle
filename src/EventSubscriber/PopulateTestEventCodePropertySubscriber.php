2ca696d7cbd24f723632c6f18cbd20a6093a9e74
Joachim Løvgaard <joachim@loevgaard.dk>
2022-07-04 09:42:25 +0200

    Allow to set test event code on a per session basis

---
+++ src/EventSubscriber/PopulateTestEventCodePropertySubscriber.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Setono\MetaConversionsApiBundle\EventSubscriber;
+
+use Setono\MetaConversionsApiBundle\Event\ConversionApiEventRaised;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Exception\SessionNotFoundException;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+final class PopulateTestEventCodePropertySubscriber implements EventSubscriberInterface
+{
+    private RequestStack $requestStack;
+
+    public function __construct(RequestStack $requestStack)
+    {
+        $this->requestStack = $requestStack;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            ConversionApiEventRaised::class => ['populate', 800],
+        ];
+    }
+
+    public function populate(ConversionApiEventRaised $event): void
+    {
+        try {
+            $session = $this->requestStack->getSession();
+        } catch (SessionNotFoundException $e) {
+            return;
+        }
+
+        $testEventCode = $session->get('smca_test_event_code');
+        if (!is_string($testEventCode)) {
+            return;
+        }
+
+        $event->event->testEventCode = $testEventCode;
+    }
+}
---
+++ src/EventSubscriber/StoreTestEventCodeSubscriber.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Setono\MetaConversionsApiBundle\EventSubscriber;
+
+use Setono\MainRequestTrait\MainRequestTrait;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+final class StoreTestEventCodeSubscriber implements EventSubscriberInterface
+{
+    use MainRequestTrait;
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::REQUEST => ['store', 1100],
+        ];
+    }
+
+    public function store(RequestEvent $event): void
+    {
+        if (!$this->isMainRequest($event)) {
+            return;
+        }
+
+        $request = $event->getRequest();
+
+        $testEventCode = $request->query->get('testEventCode') ?? $request->query->get('test_event_code');
+        if (!is_string($testEventCode)) {
+            return;
+        }
+
+        $request->getSession()->set('smca_test_event_code', $testEventCode);
+    }
+}
