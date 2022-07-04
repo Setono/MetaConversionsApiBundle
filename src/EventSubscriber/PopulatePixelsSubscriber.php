a4ed265929a6521dc9341dec994796a01fd7fff7
Joachim Løvgaard <joachim@loevgaard.dk>
2022-07-04 10:53:40 +0200

    Add subscriber that populates pixels to event

---
+++ src/EventSubscriber/PopulatePixelsSubscriber.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Setono\MetaConversionsApiBundle\EventSubscriber;
+
+use Setono\MetaConversionsApiBundle\Event\ConversionApiEventRaised;
+use Setono\MetaConversionsApiBundle\Provider\PixelProviderInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Exception\SessionNotFoundException;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+final class PopulatePixelsSubscriber implements EventSubscriberInterface
+{
+    private PixelProviderInterface $pixelProvider;
+
+    public function __construct(PixelProviderInterface $pixelProvider)
+    {
+        $this->pixelProvider = $pixelProvider;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            ConversionApiEventRaised::class => ['populate', 700],
+        ];
+    }
+
+    public function populate(ConversionApiEventRaised $event): void
+    {
+        $event->event->pixels = $this->pixelProvider->getPixels();
+    }
+}
