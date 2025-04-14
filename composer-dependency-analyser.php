05a2c4bb2c3257efe64a971ec3d2a42f79bfaf0b
Joachim Løvgaard <joachim@loevgaard.dk>
2025-04-14 16:13:41 +0200

    Fix deps

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -3,7 +3,10 @@
 declare(strict_types=1);
 
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
 return (new Configuration())
     ->addPathToExclude(__DIR__ . '/tests')
+    ->ignoreErrorsOnPackage('setono/consent-contracts', [ErrorType::SHADOW_DEPENDENCY]) // Optional dependency
+    ->ignoreErrorsOnPackage('setono/tag-bag', [ErrorType::DEV_DEPENDENCY_IN_PROD]) // Optional dependency
 ;
